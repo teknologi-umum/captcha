a90b5aa3f02fe56674a92ba72a9579e0a428a11f
Reinaldy Rafli <aldy505@tutanota.com>
2021-10-20 19:35:31 +0700

    feat: setir reply to

--- handlers/setir.go
+++ handlers/setir.go
@@ -17,16 +17,47 @@ func (d *Dependencies) SetirManual(m *tb.Message) {
 	if err != nil {
 		panic(tracerr.Wrap(err))
 	}
+
+	if m.IsReply() {
+		replyToID, err := strconv.Atoi(m.Payload)
+		if err != nil {
+			panic(tracerr.Wrap(err))
+		}
+		_, err = d.Bot.Send(tb.ChatID(home), m.ReplyTo.Text, &tb.SendOptions{
+			ParseMode: tb.ModeHTML,
+			AllowWithoutReply: true,
+			ReplyTo: &tb.Message{
+				ID: replyToID,
+				Chat: &tb.Chat{
+					ID: int64(home),
+				},
+			},
+		})
+		if err != nil {
+			_, err = d.Bot.Send(m.Chat, "Failed sending that message: " + err.Error())
+			if err != nil {
+				panic(tracerr.Wrap(err))
+			}
+		} else {
+			_, err = d.Bot.Send(m.Chat, "Message sent")
+			if err != nil {
+				panic(tracerr.Wrap(err))
+			}
+		}
+		return
+	}
+
 	_, err = d.Bot.Send(tb.ChatID(home), m.Payload, &tb.SendOptions{ParseMode: tb.ModeHTML, AllowWithoutReply: true})
 	if err != nil {
 		_, err = d.Bot.Send(m.Chat, "Failed sending that message: " + err.Error())
 		if err != nil {
 			panic(tracerr.Wrap(err))
 		}
-	} else {
-		_, err = d.Bot.Send(m.Chat, "Message sent")
-		if err != nil {
-			panic(tracerr.Wrap(err))
-		}
+		return
+	}
+
+	_, err = d.Bot.Send(m.Chat, "Message sent")
+	if err != nil {
+		panic(tracerr.Wrap(err))
 	}
 }
