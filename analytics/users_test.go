88a3b52c7026213a54fd0f50b1aec3ac1036b4a3
Reinaldy Rafli <aldy505@tutanota.com>
2021-11-30 05:19:20 +0000

    test: missing flush all users id

--- analytics/users_test.go
+++ analytics/users_test.go
@@ -67,3 +67,33 @@ func TestGetAllUserMap(t *testing.T) {
 		t.Error("Expected 3 users, got ", len(users))
 	}
 }
+
+func TestFlushAllUserID(t *testing.T) {
+	defer Cleanup()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+
+	tx := cache.TxPipeline()
+	defer tx.Close()
+	tx.SAdd(ctx, "analytics:users", "1", "2", "3")
+	tx.HSet(ctx, "analytics:1", "username", "adam", "display_name", "Adam", "counter", 1)
+	tx.HSet(ctx, "analytics:2", "username", "bobby45", "display_name", "Bobby", "counter", 5)
+	tx.HSet(ctx, "analytics:3", "username", "clifford77", "display_name", "Clifford", "counter", 3)
+
+	_, err := tx.Exec(ctx)
+	if err != nil {
+		t.Error(err)
+	}
+
+	deps := &analytics.Dependency{
+		DB:     db,
+		Redis:  cache,
+		Memory: memory,
+	}
+
+	err = deps.FlushAllUserID(ctx)
+	if err != nil {
+		t.Error(err)
+	}
+}
