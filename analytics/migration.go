29325b277b9f02c7da485f54d2652e314f210afd
Reinaldy Rafli <aldy505@tutanota.com>
2021-12-05 14:56:38 +0700

    chore: changed to timestamp

--- analytics/migration.go
+++ analytics/migration.go
@@ -60,7 +60,7 @@ func (d *Dependency) Migrate() error {
 	_, err = t.ExecContext(
 		ctx,
 		`CREATE TABLE IF NOT EXISTS analytics_hourly (
-			todays_date 		DATETIME 	PRIMARY KEY,
+			todays_date 		TIMESTAMP 	PRIMARY KEY,
 			zero_hour 			INTEGER 	DEFAULT 0,
 			one_hour 			INTEGER 	DEFAULT 0,
 			two_hour 			INTEGER 	DEFAULT 0,
