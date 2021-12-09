deced45012b88f7bfcad5c6bf63a69dd70f9b62d
Reinaldy Rafli <aldy505@tutanota.com>
2021-12-09 09:02:00 +0700

    test: fix wrong assertion value

--- analytics/parser_test.go
+++ analytics/parser_test.go
@@ -31,7 +31,7 @@ func TestParseToUser(t *testing.T) {
 	if userMap.Username != "reinaldy" {
 		t.Errorf("Username should be reinaldy, got: %s", userMap.Username)
 	}
-	if userMap.GroupID.Int64 != 123456 {
+	if userMap.GroupID.Int64 != 123456789 {
 		t.Errorf("GroupID should be 123456789, got: %d", userMap.GroupID.Int64)
 	}
 }
