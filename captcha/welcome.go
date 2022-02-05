cc06a4986f07b5db32dbe8d2191e75feb316ca19
raymond <180709879@students.uajy.ac.id>
2022-02-06 05:52:45 +0700

    [RMV] remove index 5, 8, 9. as requested

--- captcha/welcome.go
+++ captcha/welcome.go
@@ -16,7 +16,7 @@ import (
 // that have a dynamic user value, written as {user}.
 //
 // This should be sent to the user with a random pick.
-var currentWelcomeMessages = [10]string{
+var currentWelcomeMessages = [7]string{
 	`Halo, {user}!
 
 Selamat datang di grup Teknologi Umum. Disini kita berisik banget, jadi langsung matiin notificationnya ya.
@@ -46,10 +46,6 @@ Selamat datang di grup Teknologi Umum!
 Coba ketik (dan kirim) /quiz deh, nanti grup ini tiba-tiba hidup.
 
 Oh iya, grup ini ngga ada aturan. Tapi jangan sampe bikin kita diciduk tukang bakso bawa HT.`,
-	`Wah mantap, {user} bisa nyelesain captcha yang aneh itu.
-
-Di grup ini nggak usah sungkan-sungkan kalau mau tanya atau bicara ya. Kita semua manusia bar-bar.
-Sehari bisa muncul lebih dari 500 message, jadi jangan lupa matiin notifikasi ya!`,
 	`Haii {user}!
 
 Selama di grup ini, jangan sungkan & malu-malu ya. Biarin aja grup ini berisik. Jangan lupa matiin notification juga.
@@ -64,30 +60,7 @@ Banyak member grup ini yang udah kerja di tempat-tempat keren, dan mereka juga s
 Github. Pastiin profile Github-mu isinya project yang keren juga ya! Nggak usah malu-malu kalau menurutmu masih
 biasa aja :D
 
-Jangan lupa matiin notifikasi, grup ini berisik banget, apalagi kalo lagi ngegibah.`,
-	`Keren, captchanya selesai! Gimana kabarnya {user}?
-
-Semoga hari ini bukan hari yang buruk ya. Apalagi abis masuk grup ini, rasanya kayak bakal kenal sama temen-temen
-baru yang nggak berhenti-berhentinya ngomongin topik yang random banget. Kadang ngomongin pemrograman/teknologi,
-kadang juga ngomongin makanan atau ngomongin seberapa ngeselinnya satu provider internet yang berawalan dengan
-Indi dan berakhiran dengan home.
-
-Nah, karena kita nggak ada berhenti-berhentinya, jangan lupa matiin notification ya biar nggak berasa artis.
-
-Fun fact: ada orang yang pernah mencoba kayak gitu, jadi notificationnya dibiarin nyala. Dia hanya bertahan 3 jam.
-Abis itu memutuskan untuk mematikan notifikasinya.`,
-	`Hai {user}!
-
-Nama grup ini Teknologi Umum. Berarti kita membicarakan hal berbau teknologi dan/atau hal umum.
-
-Jadi kalo ada yang lagi ngomongin cara terbaik untuk ngoding di PHP tapi 10 detik kemudian ada yang ngomong,
-"ini kenapa angkot depan rumah ngeselin banget sih", itu valid. Nggak ada topik OOT disini.
-
-Kamu nggak perlu pasang profile picture/username. Cukup matiin notification aja, berisik banget disini.
-
-Kita juga punya website dan Github organization. Bisa di cek di: https://teknologiumum.com
-dan https://github.com/teknologi-umum`,
-}
+Jangan lupa matiin notifikasi, grup ini berisik banget, apalagi kalo lagi ngegibah.`}
 
 // deleteMessage creates a timer of one minute to delete a certain message.
 func deleteMessage(bot *tb.Bot, message tb.StoredMessage, logger *sentry.Client) {
