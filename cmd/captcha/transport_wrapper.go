6b5e9cdd94389245c8160cbf5a37c5d2760c0dbb
Reinaldy Rafli <aldy505@proton.me>
2023-12-31 19:49:20 +0700

    feat: enable http transport tracing with sentry

---
+++ cmd/captcha/transport_wrapper.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"slices"
+	"strconv"
+	"strings"
+
+	"github.com/getsentry/sentry-go"
+)
+
+type SentryTransportWrapper struct {
+	OriginalTransport *http.Transport
+}
+
+func (s *SentryTransportWrapper) RoundTrip(request *http.Request) (*http.Response, error) {
+	ignoredMethods := []string{"getme", "logout", "close", "getupdates"}
+	if slices.Contains(ignoredMethods, strings.ToLower(request.URL.Path)) {
+		return s.OriginalTransport.RoundTrip(request)
+	}
+
+	// Start Sentry trace
+	ctx := request.Context()
+	var cleanRequestURL string
+	pathFragments := strings.Split(request.URL.Path, "/")
+	if len(pathFragments) >= 2 {
+		if pathFragments[0] == "" {
+			pathFragments[1] = "bot[Filtered]"
+		} else {
+			pathFragments[0] = "bot[Filtered]"
+		}
+
+		cleanRequestURL = strings.Join(pathFragments, "/")
+	}
+
+	span := sentry.StartSpan(ctx, "http.client", sentry.WithTransactionName(fmt.Sprintf("%s %s", request.Method, cleanRequestURL)))
+	defer span.Finish()
+
+	span.SetData("http.query", request.URL.Query().Encode())
+	span.SetData("http.fragment", request.URL.Fragment)
+	span.SetData("http.request.method", request.Method)
+
+	response, err := s.OriginalTransport.RoundTrip(request)
+
+	span.SetData("http.response.status_code", response.Status)
+	span.SetData("http.response_content_length", strconv.FormatInt(response.ContentLength, 10))
+
+	return response, err
+}
